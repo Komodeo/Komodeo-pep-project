0902d35ea0b8ddcf091aa21de0ef80cac1dc0e79
Grant Brown <onlythebest@gmail.com>
2024-07-23 23:11:39 +0000

    registration complete

--- src/main/java/Controller/SocialMediaController.java
+++ src/main/java/Controller/SocialMediaController.java
@@ -1,33 +1,81 @@
 package Controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import Model.Account;
+import Service.AccountService;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
 
 /**
- * TODO: You will need to write your own endpoints and handlers for your controller. The endpoints you will need can be
+ * TODO: You will need to write your own endpoints and handlers for your
+ * controller. The endpoints you will need can be
  * found in readme.md as well as the test cases. You should
- * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
+ * refer to prior mini-project labs and lecture materials for guidance on how a
+ * controller may be built.
  */
 public class SocialMediaController {
+    // map account service
+    AccountService accountService;
+    public SocialMediaController(){
+        accountService = new AccountService();
+    }
     /**
-     * In order for the test cases to work, you will need to write the endpoints in the startAPI() method, as the test
+     * In order for the test cases to work, you will need to write the endpoints in
+     * the startAPI() method, as the test
      * suite must receive a Javalin object from this method.
-     * @return a Javalin app object which defines the behavior of the Javalin controller.
+     * 
+     * @return a Javalin app object which defines the behavior of the Javalin
+     *         controller.
      */
     public Javalin startAPI() {
         Javalin app = Javalin.create();
-        app.get("example-endpoint", this::exampleHandler);
+        app.post("/register", this::registerHandler);
 
         return app;
     }
 
     /**
+     * Handler to register a new user.
+     * ## 1: Our API should be able to process new User registrations.
+     * 
+     * As a user, I should be able to create a new Account on the endpoint POST
+     * localhost:8080/register. The body will contain a representation of a JSON
+     * Account, but will not contain an account_id.
+     * 
+     * - The registration will be successful if and only if the username is not
+     * blank, the password is at least 4 characters long, and an Account with that
+     * username does not already exist. If all these conditions are met, the
+     * response body should contain a JSON of the Account, including its account_id.
+     * The response status should be 200 OK, which is the default. The new account
+     * should be persisted to the database.
+     * - If the registration is not successful, the response status should be 400.
+     * (Client error)
+     * 
+     * @param ctx The Javalin Context object manages information about both the
+     *                HTTP request and response.
+     */
+    private void registerHandler(Context ctx) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        Account account = mapper.readValue(ctx.body(), Account.class);
+        Account addedAccount = accountService.addAccount(account);
+        if (addedAccount==null || addedAccount.getUsername()=="" || addedAccount.getPassword().length() < 4) {
+            ctx.status(400);
+        }
+        else {
+            ctx.json(mapper.writeValueAsString(addedAccount));
+        }
+    }
+
+    /**
      * This is an example handler for an example endpoint.
-     * @param context The Javalin Context object manages information about both the HTTP request and response.
+     * 
+     * @param context The Javalin Context object manages information about both the
+     *                HTTP request and response.
      */
     private void exampleHandler(Context context) {
         context.json("sample text");
     }
-
 
 }
---
+++ src/main/java/Service/AccountService.java
@@ -0,0 +1,36 @@
+package Service;
+
+import DAO.AccountDAO;
+import Model.Account;
+
+public class AccountService {
+    // map AccountDAO
+    AccountDAO accountDAO;
+
+    // No-args constructor for an accountService instantiates a plain AccountDAO
+    public AccountService(){
+        accountDAO = new AccountDAO();
+    }
+
+    /**
+     * Constructor for an accountService when an accountDAO is provided.
+     * This is used for when a mock accountDAO that exhibits mock behavior is used in the test cases.
+     * This would allow the testing of AccountService independently of AccountDAO.
+     * @param accountDAO
+     */
+    public AccountService(AccountDAO accountDAO){
+        this.accountDAO = accountDAO;
+    }
+
+    /*
+     * TODO: Use the AccountDAO to add (register) a new account to the database.
+     * 
+     * This method should also return the added account.
+     * 
+     * @param account an object representing a new Account.
+     * @return the newly added account if the add operation was successful
+     */
+    public Account addAccount(Account account){
+        return accountDAO.insertAccount(account);
+    }
+}
